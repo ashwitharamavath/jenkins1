4fc5f82c8ae46d5819487e99cc8549568d1c7ffe
Ashwitha <168261652+ashwitharamavath@users.noreply.github.com>
2025-09-12 14:18:23 +0530

    Update Hello.java

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -3,6 +3,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins Pipeline CI/CD!");
         System.out.println("Hello Ashwitha");
+        System.out.println("Webhook");
     }
 }
 
